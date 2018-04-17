5b98494163fa8fe353bfd3f15a9c9c8212716045
daniro-13ftf <38410626+daniro-13ftf@users.noreply.github.com>
2018-04-18 00:47:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 # just to check
 for i in ['Gleb Fedorovich', 'Ustimenko Nikita', 'Pashina Lesya', 'Poleva Maria', 'Ryabov Danil', 'Artyom Kuznetsov']:
   print (i ' is the member of the coolest team SpaceY', end ='\n')
+#some new changes
