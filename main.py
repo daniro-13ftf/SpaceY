7c0724965ee4b801ce6502883bfd7555ff279370
daniro-13ftf <rdr.300799@gmail.com>
2018-04-19 00:05:42 +0300

    Исправил кусочек кода

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # just to check
 for i in ['Gleb Fedorovich', 'Ustimenko Nikita', 'Pashina Lesya', 'Poleva Maria', 'Ryabov Danil', 'Artyom Kuznetsov']:
-  print (i ' is the member of the coolest team SpaceY', end ='\n')
+  print (i +' is the member of the coolest team SpaceY', end ='\n')
